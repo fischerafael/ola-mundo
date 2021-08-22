43cb40e2aa566bd41430c74233fe3360df29bc39
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-22 19:42:24 -0300

    change name

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => (
     <section className="max-w-screen-md w-full flex justify-between">
         <p className="text-base">
-            <span className="font-bold">fischer</span>afael
+            <span className="font-bold">ola</span>mundo
         </p>
     </section>
 );
